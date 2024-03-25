0f5dca3a0d4307533130e4bb7d70d2b750cf3b9b
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-03-25 00:45:34 -0400

    Main

---
+++ pdfFieldReader.py
@@ -0,0 +1,27 @@
+# MAIN LOGIC
+import PyPDF2
+import test_path
+
+def extract_pdf_fields(file_path):
+    with open(file_path, 'rb') as document:
+        doc_reader = PyPDF2.PdfFileReader(document)
+        fields_dict = {}
+
+        for page_num in range(doc_reader.numPages):
+            page = doc_reader.getPage(page_num)
+            try:
+                fields = page['/Annots']
+                for field in fields:
+                    field_obj = field.getObject()
+                    if field_obj.get('/FT') == '/Tx':
+                        field_name = field_obj.get('/T')
+                        field_value = field_obj.get('/V')
+                        fields_dict[field_name] = field_value
+            except KeyError:
+                pass
+        return fields_dict
+
+# fields_dict = extract_pdf_fields(test_path.file)
+# with open("output.txt", "w") as f:
+#     for field_name, field_value in fields_dict.items():
+#         print(f"Field: {field_name}, Value: {field_value}", file=f)
