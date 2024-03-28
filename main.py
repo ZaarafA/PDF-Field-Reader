dd300d8ab962c1d2d6ecf32fbe2bdf2cacb49ce4
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-03-28 00:05:52 -0400

    Window Layout

--- main.py
+++ main.py
@@ -4,27 +4,37 @@ from tkinter import filedialog
 from tkinter import ttk
 import sv_ttk
 
+filepath = ''
+
+# opens a file dialog and stores the selected file path
 def openFile():
+    global filepath
     filepath = filedialog.askopenfilename()
-    intro_text.delete('1.0', tk.END)
-    intro_text.insert(tk.END, filepath)
+    print(filepath)
 
 # window
 window = tk.Tk()
 window.title("PDF Field Reader")
 window.geometry("800x600")
+window.resizable(False, False)
 
 # Widgets
-button = ttk.Button(window, text="Click")
-intro_text = tk.Text(window)
+intro_label = tk.Label(window, width=300, height=50,wraplength=200,
+                        text="This tool reads the fields in a pdf and allows you to export it into a file")
+display_text = tk.Text(window)
 select_button = ttk.Button(text="SELECT PDF", command=openFile)
 saveTXT_button = ttk.Button(text="SAVE TO TXT")
 saveCSV_button = ttk.Button(text="SAVE TO CSV")
 saveJSON_button = ttk.Button(text="SAVE TO JSON")
 
-select_button.pack()
-intro_text.pack()
-intro_text.insert(tk.END, "Hello, World!")
+# Layout
+intro_label.place(x=25,y=10,width=350,height=50)
+display_text.place(width=390,height=550,x=390,y=10)
+select_button.place(width=350,height=50,x=15,y=70)
+saveTXT_button.place(width=250,x=65,y=135)
+saveCSV_button.place(width=250,x=65,y=175)
+saveJSON_button.place(width=250,x=65,y=215)
+
 
 # window init
 sv_ttk.set_theme("dark")
